72201a347f1fe6cdd9ffe719031c9ce017cbc546
Daniel Dutton <danielbdutton@googlemail.com>
2013-11-12 19:42:44 +0000

    Merge Sort Implementation Added

--- Sorter.Algorithms/Routines/MergeSort.cs
+++ Sorter.Algorithms/Routines/MergeSort.cs
@@ -1,12 +1,47 @@
-﻿using System;
-
-namespace Sorter.Algorithms.Routines
+﻿namespace Sorter.Algorithms.Routines
 {
     public sealed class MergeSort : SortRoutine
     {
         public override int[] Sort(int[] data)
         {
-            throw new NotImplementedException();
+            if (data.Length == 1)
+                return data;
+            
+            int middle = data.Length / 2;
+            int[] left = new int[middle];
+            
+            for (int i = 0; i < middle; i++)
+            {
+                left[i] = data[i];
+            }
+            
+            int[] right = new int[data.Length - middle];
+            
+            for (int i = 0; i < data.Length - middle; i++)
+            {
+                right[i] = data[i + middle];
+            }
+            left = Sort(left);
+            right = Sort(right);
+
+            int leftptr = 0;
+            int rightptr = 0;
+
+            int[] sorted = new int[data.Length];
+            for (int k = 0; k < data.Length; k++)
+            {
+                if (rightptr == right.Length || ((leftptr < left.Length) && (left[leftptr] <= right[rightptr])))
+                {
+                    sorted[k] = left[leftptr];
+                    leftptr++;
+                }
+                else if (leftptr == left.Length || ((rightptr < right.Length) && (right[rightptr] <= left[leftptr])))
+                {
+                    sorted[k] = right[rightptr];
+                    rightptr++;
+                }
+            }
+            return sorted;
         }
     }
 }
